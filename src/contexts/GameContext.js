413038393c16b396897eb44dadf2a73c2ddeebab
Sammy.p <samueledi80@hotmail.com>
2019-11-02 18:39:16 +0100

    Local storage added

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -1,11 +1,16 @@
-import React, {createContext, useReducer } from 'react';
+import React, {createContext, useReducer, useEffect } from 'react';
 import { gameReducer } from '../reducers/gameReducer';
 
 export const GameContext = createContext();
 
 const GameContextProvider = props => {
-    const [ games, dispatch ] = useReducer(gameReducer, []);
-
+    const [ games, dispatch ] = useReducer(gameReducer, [], () => {
+        const localData = localStorage.getItem('games')
+        return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+        localStorage.setItem('games', JSON.stringify(games))
+    }, [games]);
     return (
         <GameContext.Provider value={{ games, dispatch}}>
             {props.children}
